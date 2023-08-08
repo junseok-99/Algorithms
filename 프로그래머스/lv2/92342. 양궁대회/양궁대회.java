9a274da364705f53781a215e4fc2c694e55690e7
장준석 <81612834+junseok-99@users.noreply.github.com>
2023-08-08 21:30:59 +0900

    [level 2] Title: 양궁대회, Time: 0.95 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/92342. 양궁대회/양궁대회.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    
+    int max, ans[], apeach[];
+
+    void find(int n, int cur) {
+        int score = 0, state[] = new int[11];
+        for(int i=1; i<=10; i++) {
+            if((cur&1<<i) > 0) {
+                n -= state[10-i] = apeach[10-i]+1;
+                if(n < 0) return;
+                score += i;
+            }else if(apeach[10-i] > 0) score -= i;
+        }
+        if(score <= 0) return;
+        state[10] = n;
+        if(max < score) {
+            max = score;
+            ans = state;
+        }else if(max == score) {
+            for(int i=10; i>=0; i--) {
+                if(ans[i] != state[i]) {
+                    if(state[i] > ans[i]) 
+                        ans = state;
+                    return;
+                }
+            }
+        }
+    }
+    int[] solution(int n, int[] info) {
+        apeach = info;
+        for(int i=1; i<1<<11; i++)
+            if(Integer.bitCount(i) <= n)
+                find(n, i);
+
+        return max == 0 ? new int[] {-1} : ans;
+    }
+}
