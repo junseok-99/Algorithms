1ee73ca505435ad1a38e9c410728f7db788dcaad
장준석 <81612834+junseok-99@users.noreply.github.com>
2023-08-19 02:21:04 +0900

    [level 3] Title: 야근 지수, Time: 140.79 ms, Memory: 69.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12927. 야근 지수/야근 지수.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] works) {
+        long answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        
+        for(int i=0;i<works.length;i++) {
+            pq.add(works[i]);
+        }
+        
+        while (n > 0) {
+            Integer maxHour = pq.poll();
+            
+            if (maxHour == 0) {
+                return 0;
+            }
+            
+            if (pq.isEmpty()) {
+                maxHour = maxHour - n < 0 ? 0 : maxHour - n;
+                break;
+            } else {
+                while (maxHour >= pq.peek() && maxHour > 0 && n > 0) {
+                    maxHour--;
+                    n--;
+                }
+            }
+            
+            pq.add(maxHour);
+        }
+        
+        while (!pq.isEmpty()) {
+            int num = pq.poll();
+            answer += (long)Math.pow(num, 2);
+        }
+        
+        return answer;
+    }
+}
