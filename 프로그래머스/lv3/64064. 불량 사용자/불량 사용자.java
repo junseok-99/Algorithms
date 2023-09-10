9244a0e93268e931e0d8249520c0536f44b7dde0
장준석 <81612834+junseok-99@users.noreply.github.com>
2023-09-10 21:46:17 +0900

    [level 3] Title: 불량 사용자, Time: 3.10 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
+++ 프로그래머스/lv3/64064. 불량 사용자/불량 사용자.java
@@ -1,72 +1,49 @@
 import java.util.*;
 
 class Solution {
-    int userSize;
-    int bannedSize;
-    int answer = 0;
-    Set<String> set = new HashSet<>();
-    boolean[] visited;
-    
-    public void dfs(int depth, String[] user_id, String[] banned_id, String str) {
-        if (depth == userSize) {
-            String[] tmp = str.split(" ");
-            int cnt = 0;
-            StringBuilder sb = new StringBuilder();
-            for(int j=0;j<userSize;j++) {
-                if (checkId(tmp[j], banned_id[cnt])) {
-                    ++cnt;
-                    sb.append(tmp[j] + " ");
-                }
-                if (cnt == bannedSize) {
-                    sb.deleteCharAt(sb.length() - 1);
-                    String s = makeId(sb.toString().split(" "));
-                    if (!set.contains(s)) {
-                        ++answer;
-                        set.add(s);
-                    }
-                    break;
-                }
-            }
-        }
-        for(int i=0;i<userSize;i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                dfs(depth + 1, user_id, banned_id, str + user_id[i] + " ");
-                visited[i] = false;
-            }
-        }
-    }
-    
+    static HashSet<HashSet<String>> answer;
     public int solution(String[] user_id, String[] banned_id) {
-        bannedSize = banned_id.length;
-        userSize = user_id.length;
-        visited = new boolean[userSize];
-        
-        dfs(0, user_id, banned_id, "");
-        
-        return answer;
+        answer = new HashSet<>();
+
+        dfs(new LinkedHashSet<>(), user_id, banned_id);
+
+        return answer.size();
     }
-    
-    public String makeId(String[] arr) {
-        Arrays.sort(arr);
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<arr.length;i++) {
-            sb.append(arr[i]);
+
+
+    private static void dfs(HashSet<String> hs, String[] user_id, String[] banned_id) {
+        if (hs.size() == banned_id.length) {
+            if (isBanList(hs, banned_id)) {
+                answer.add(new HashSet<>(hs));
+            }
+            return;
         }
-        return sb.toString();
+
+        for (String userId : user_id) {
+            if (hs.add(userId)) {
+                dfs(hs, user_id, banned_id);
+                hs.remove(userId);
+            }
+        }
     }
-    
-    public boolean checkId(String s1, String s2) {
-        if (s1.length() == s2.length()) {
-            for(int i=0;i<s1.length();i++) {
-                if (s2.charAt(i) == '*') {
+
+
+    private static boolean isBanList(HashSet<String> hs, String[] banned_id) {
+        int idx = 0;
+        for (String userID : hs) {
+            String banID = banned_id[idx++];
+            if (userID.length() != banID.length()) {
+                return false;
+            }
+            for (int i = 0; i < banID.length(); i++) {
+                if (banID.charAt(i) == '*') {
                     continue;
-                } else if (s1.charAt(i) != s2.charAt(i)) {
+                }
+                if (userID.charAt(i) != banID.charAt(i)) {
                     return false;
                 }
             }
-            return true;
         }
-        return false;
+        return true;
     }
 }
