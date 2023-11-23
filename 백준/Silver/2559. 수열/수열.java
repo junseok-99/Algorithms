7346a4ef71a9d27a6f41a516d02a17bd57d9c119
장준석 <angec5689@naver.com>
2023-11-23 21:48:00 +0900

    [Silver III] Title: 수열, Time: 104 ms, Memory: 13680 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,20 +1,18 @@
 import java.io.*;
-import java.util.*;
 
-public class Main {
+public class Main extends FastInput {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        initFI();
+        int N = nextInt();
+        int K = nextInt();
         int max = Integer.MIN_VALUE;
         int[] arr = new int[N+1];
         int[] arr2 = new int[N+1];
-        st = new StringTokenizer(br.readLine());
+
         for (int i=1;i<=N;i++) {
-            int n = Integer.parseInt(st.nextToken());
+            int n = nextInt();
             arr[i] = arr[i-1] + n;
 
         }
@@ -23,5 +21,38 @@ public class Main {
             max = Math.max(max, arr2[i]);
         }
         System.out.println(max);
+    }
+}
+class FastInput {
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 20;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    protected static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    protected static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
     }
 }
