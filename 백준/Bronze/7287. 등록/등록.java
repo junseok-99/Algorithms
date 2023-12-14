2374fa468c29199229a009c531d07b1de96fb257
장준석 <angec5689@naver.com>
2023-12-14 16:27:16 +0900

    [Bronze V] Title: 등록, Time: 72 ms, Memory: 11192 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("213");
+        System.out.println("angec5647");
+    }
+}
