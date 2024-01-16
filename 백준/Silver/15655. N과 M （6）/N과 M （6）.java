67e6dd6c47f1fdc270d29679475d0891c3820203
장준석 <angec5689@naver.com>
2024-01-16 17:07:29 +0900

    [Silver III] Title: N과 M (6), Time: 80 ms, Memory: 11680 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	private static int[] arr;
+	private static boolean[] visited;
+	
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        visited = new boolean[N];
+        st = new StringTokenizer(br.readLine());
+        
+        for (int i = 0; i < N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        bt(N, M, 0, 0, "");
+        bw.flush();
+        bw.close();
+    }
+    
+    public static void bt(int N, int M, int n, int depth, String str) throws IOException {
+    	if (depth >= M) {
+    		if (depth == M) {
+    			bw.write(str + "\n");
+    		}
+    		return;
+    	}
+    	
+    	for (int i = n; i < N; i++) {
+    		if (!visited[i]) {
+    			visited[i] = true;
+    			bt(N, M, i + 1, depth + 1, str + arr[i] + " ");
+    			visited[i] = false;
+    		}
+    	}
+    }
+}
