0f9b10548a6d32cb083881098b1ab56bf8e32185
장준석 <angec5689@naver.com>
2024-02-06 14:30:39 +0900

    [Bronze II] Title: 블랙잭, Time: 128 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,42 +1,38 @@
 import java.io.BufferedReader;
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] arr;
-	static int N;
-	static int M;
-	static int answer = Integer.MIN_VALUE;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		combi(0, 0, 0);
-		
-		System.out.println(answer);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-	public static void combi(int idx, int depth, int sum) {
-		if (depth == 3) {
-			if (sum <= M) {
-				answer = Math.max(answer, sum);
-			}
-			return;
-		}
-		
-		for (int i = idx; i < N; i++) {
-			combi(i + 1, depth + 1, sum + arr[i]);
-		}
-	}
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] cards = new int[N];
+        int max = 1;
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0;i<N;i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0;i<=N-3;i++) {
+            for(int j=i+1;j<N;j++) {
+                for(int k=j+1;k<N;k++) {
+                    int sum = cards[i] + cards[j] + cards[k];
+                    if (sum <= M && max < sum) {
+                        max = sum;
+                    }
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+
 }
