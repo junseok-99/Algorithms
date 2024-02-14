bca3a319471a492e7fa1915c9f825cc3bb2eb4b2
장준석 <angec5689@naver.com>
2024-02-14 23:42:26 +0900

    [Silver III] Title: 주유소, Time: 424 ms, Memory: 35504 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,15 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static Set<Integer> set;
-
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -27,13 +22,11 @@ public class Main {
             cost[i] = Integer.parseInt(st.nextToken());
         }
         int minCost = cost[0];
-        int sumOil = dist[0] * cost[0];
+        long sumOil = dist[0] * (long)cost[0];
 
         for (int i = 1; i < N - 1; i++) {
-            if (cost[i] < minCost) {
-                minCost = cost[i];
-            }
-            sumOil += dist[i] * minCost;
+            minCost = Math.min(minCost, cost[i]);
+            sumOil += (dist[i] * (long)minCost);
         }
         System.out.println(sumOil);
     }
