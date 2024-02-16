83e8730fb89d0edf62fea19be1da11ce13f187f3
장준석 <angec5689@naver.com>
2024-02-16 15:58:37 +0900

    [Gold V] Title: 토마토, Time: 632 ms, Memory: 104416 KB -BaekjoonHub

--- 백준/Gold V/7576. 토마토/토마토.java
+++ 백준/Gold V/7576. 토마토/토마토.java
@@ -1,76 +1,86 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
-    public static class Pos{
-        int x;
-        int y;
-        Pos(int x, int y){
-            this.x = x;
-            this.y = y;
-        }
-    }
-    public static int[][] table;
-    public static int notomato = 0;
-    public static Queue<Pos> q = new LinkedList<>();
-    public static int bfs(int m, int n){
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,1,-1};
-        Queue<Queue> q2 = new LinkedList<>();
-        q2.add(q);
-        int cnt = -1;
-        while(!q2.isEmpty()) {
-            q2.poll();
-            int size = q.size();
-            for(int j=0;j<size;j++) {
-                Pos p = q.poll();
-                for (int i = 0; i < 4; i++) {
-                    int tx = p.x + dx[i];
-                    int ty = p.y + dy[i];
-                    if (tx < m && tx >= 0 && ty < n && ty >= 0) {
-                        if (table[ty][tx] == 0) {
-                            table[ty][tx] = 1;
-                            notomato--;
-                            q.add(new Pos(tx, ty));
-                        }
-                    }
-                }
-            }
-            if(q.size() > 0) q2.add(q);
-            cnt++;
-        }
-        return notomato > 0 ? -1 : cnt;
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int M;
+	static int N;
+	static int[][] map;
+	static int[][] d = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+	static Deque<Point> ripeTomatoQ = new ArrayDeque<>();
+	static boolean[][] visited;
+	static int rawTomato;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		map = new int[N][M];
+		visited = new boolean[N][M];
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 1) {
+					ripeTomatoQ.add(new Point(i, j));
+					visited[i][j] = true;
+				} else if (map[i][j] == 0) {
+					rawTomato++;
+				}
+			}
+		}
+		
+		int answer = bfs();
+		if (rawTomato > 0) {
+			System.out.println(-1);
+		} else {
+			System.out.println(answer);
+		}
+	}
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+	//토마토 익히기
+	public static int bfs() {
+		
+		int day = -1;
+		
+		while (!ripeTomatoQ.isEmpty()) {
+			int size = ripeTomatoQ.size();
+			while (size-- > 0) {
+				Point p = ripeTomatoQ.poll();
+				
+				for (int i = 0; i < 4; i++) {
+					int ty = p.y + d[i][0];
+					int tx = p.x + d[i][1];
+					
+					if (tx < 0 || tx >= M || ty < 0 || ty >= N || visited[ty][tx]) {
+						continue;
+					}
+					if (map[ty][tx] == 0) {
+						--rawTomato;
+						visited[ty][tx] = true;
+						map[ty][tx] = 1;
+						ripeTomatoQ.add(new Point(ty, tx));
+					}
+				}
+			}	
+			++day;
+		}
+		return day;
+	}
+}
 
-        table = new int[n][m];
-
-        for(int i=0;i<n;i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0;j<m;j++){
-                int t = Integer.parseInt(st.nextToken());
-                if(t==0) ++notomato;
-                else if(t==1) q.add(new Pos(j,i));
-                table[i][j] = t;
-            }
-        }
-
-        if(notomato == 0){
-            bw.write(notomato+"");
-            bw.flush();
-        }
-        else{
-            bw.write(bfs(m,n)+"");
-            bw.flush();
-        }
-
-        bw.close();
-    }
+class Point {
+	int x;
+	int y;
+	
+	Point(int y, int x) {
+		this.x = x;
+		this.y = y;
+	}
 }
