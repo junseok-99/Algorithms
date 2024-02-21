9a8c70b7cc7023f58d5adb6aa0d33204317a0d91
장준석 <angec5689@naver.com>
2024-02-21 10:25:36 +0900

    [Gold IV] Title: 알파벳, Time: 4776 ms, Memory: 298528 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,51 +1,123 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
 public class Main {
 
-    private static char[][] board;
-    private static Map<Character, Boolean> visited = new HashMap<>();
-    private static int maxBlock = 0;
+	static char[][] map;
+	static boolean[][] visited;
+	static int R;
+	static int C;
+	static int[][] d = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+	static int max = 1;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		map = new char[R][C];
+		visited = new boolean[R][C];
+		
+		//맵 초기화
+		for (int i = 0; i < R; i++) {
+			map[i] = br.readLine().toCharArray();
+		}
+		
+//		System.out.println(bfs());
+		dfs(0, 0, 0, "");
+		System.out.println(max);
+	}
+	
+	public static void dfs(int y, int x, int depth, String s) {
+		
+		if (s.contains(map[y][x] + "")) {
+			max = Math.max(max, depth);
+			return;
+		}
+		
+		for (int i = 0; i < 4; i++) {
+			int tx = x + d[i][0];
+			int ty = y + d[i][1];
+			
+			if (invalidRange(ty, tx)) {
+				continue;
+			}
+			
 
-    public static void dfs(int y, int x, int n, int m, int cnt){
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
+			dfs(ty, tx, depth + 1, s + map[y][x]);
+			
+		}
+	}
+	
+	//말 움직이기
+//	public static int bfs() {
+//		Deque<Point> q = new ArrayDeque<>();
+//		
+//		int[][] d = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+//		Point sp = new Point(0, 0, 1, new HashSet<>());
+//		sp.add(map[0][0]);
+//		q.add(sp);
+//		int max = 1;
+//		
+//		while (!q.isEmpty()) {
+//			Point p = q.poll();
+//			
+//			max = Math.max(max, p.depth);
+//			
+//			for (int i = 0; i < 4; i++) {
+//				int tx = p.x + d[i][0];
+//				int ty = p.y + d[i][1];
+//				
+//				if (invalidRange(ty, tx)) {
+//					continue;
+//				}
+//				
+//				char c = map[ty][tx];
+//				if (!p.contains(c)) { //현재 알파벳을 지나오지 않았다면
+//					Set<Character> copySet = p.copySet();
+//					copySet.add(c);
+//					q.add(new Point(ty, tx, p.depth + 1, copySet));
+//				}
+//			}
+//		}
+//		return max;
+//	}
 
-        
-        maxBlock = Math.max(maxBlock, cnt);
+	public static boolean invalidRange(int y, int x) {
+		return x < 0 || x >= C || y < 0 || y >= R;
+	}
+}
 
-        for(int i=0;i<4;i++){
-            int tx = x + dx[i];
-            int ty = y + dy[i];
-            if(0 <= tx && tx < m && 0 <= ty && ty < n && !visited.get(board[ty][tx])) {
-                visited.put(board[ty][tx], true);
-                dfs(ty, tx, n, m, cnt + 1);
-                visited.put(board[ty][tx], false);
-            }
-        }
-    }
-
-    public static void main(String args[]) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
-        board = new char[n][m];
-
-        for(int i=0;i<n;i++){
-            String tmp = br.readLine();
-            for(int j=0;j<m;j++)
-                board[i][j] = tmp.charAt(j);
-        }
-
-        for(char i='A';i<='Z';i++) {
-            if(board[0][0] == i)
-                visited.put(i,true);
-            else
-                visited.put(i, false);
-        }
-
-        dfs(0,0,n,m, 1);
-
-        System.out.println(maxBlock);
-    }
+class Point {
+	Set<Character> set = new HashSet<>();
+	int x;
+	int y;
+	int depth;
+	
+	Point(int y, int x, int depth, Set<Character> set) {
+		this.x = x;
+		this.y = y;
+		this.depth = depth;
+		this.set = set;
+	}
+	
+	public boolean contains(char c) {
+		return set.contains(c);
+	}
+	
+	public void add(char c) {
+		set.add(c);
+	}
+	
+	public Set<Character> copySet() {
+		Set<Character> tmp = new HashSet<>();
+		tmp.addAll(set);
+		return tmp;
+	}
 }
