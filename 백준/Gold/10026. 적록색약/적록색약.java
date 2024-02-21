dd9ffa088bba33e8b940bf9c3bc993868e7f3594
장준석 <angec5689@naver.com>
2024-02-21 09:17:16 +0900

    [Gold V] Title: 적록색약, Time: 132 ms, Memory: 15128 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,11 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 
 public class Main {
-
 	static char[][] map;
 	static boolean[][] visited;
 	static int[][] d = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
@@ -23,94 +20,71 @@ public class Main {
 			map[i] = br.readLine().toCharArray();
 		}
 		
+		//적록색약 X
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				if (!visited[i][j]) {
-					bfs(i, j, map[i][j]);
+					dfs(i, j, map[i][j]);
 					noBlindness++;
 				}
 			}
 		}
-		
+				
+		//적록색약 O
 		visited = new boolean[N][N];
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				if (!visited[i][j]) {
-					bfs_2(i, j, map[i][j]);
+					dfs_2(i, j, map[i][j]);
 					blindness++;
 				}
 			}
-		}
-		
+		}				
+				
 		System.out.println(noBlindness + " " + blindness);
 	}
 
 	//적록색약 X
-	public static void bfs(int y, int x, char color) {
-		Deque<Point> q = new ArrayDeque<>();
-		visited[y][x] = true;
-		q.add(new Point(y, x, color));
+	public static void dfs(int y, int x, char color) {
 		
-		while (!q.isEmpty()) {
-			Point p = q.poll();
+		visited[y][x] = true;
+		
+		for (int i = 0; i < 4; i++) {
+			int tx = x + d[i][0];
+			int ty = y + d[i][1];
 			
-			for (int i = 0; i < 4; i++) {
-				int tx = p.x + d[i][0];
-				int ty = p.y + d[i][1];
-				
-				if (invalidRange(ty, tx) || visited[ty][tx]) {
-					continue;
-				}
-				
-				if (p.color == map[ty][tx]) {
-					q.add(new Point(ty, tx, p.color));
-					visited[ty][tx] = true;
-				}
+			if (invalidRange(ty, tx) || visited[ty][tx]) {
+				continue;
+			}
+			
+			if (color == map[ty][tx]) {
+				dfs(ty, tx, color);
 			}
 		}
 	}
 	
 	//적록색약 O
-	public static void bfs_2(int y, int x, char color) {
-		Deque<Point> q = new ArrayDeque<>();
-		visited[y][x] = true;
-		q.add(new Point(y, x, color));
+	public static void dfs_2(int y, int x, char color) {
 		
-		while (!q.isEmpty()) {
-			Point p = q.poll();
+		visited[y][x] = true;
+		
+		for (int i = 0; i < 4; i++) {
+			int tx = x + d[i][0];
+			int ty = y + d[i][1];
 			
-			for (int i = 0; i < 4; i++) {
-				int tx = p.x + d[i][0];
-				int ty = p.y + d[i][1];
-				
-				if (invalidRange(ty, tx) || visited[ty][tx]) {
-					continue;
-				}
-				
-				if ((p.color == 'R' || p.color == 'G') && (map[ty][tx] != 'B')) {
-					q.add(new Point(ty, tx, p.color));
-					visited[ty][tx] = true;
-				} else if (p.color == map[ty][tx]) {
-					q.add(new Point(ty, tx, p.color));
-					visited[ty][tx] = true;
-				}
+			if (invalidRange(ty, tx) || visited[ty][tx]) {
+				continue;
+			}
+			
+			if ((color == 'R' || color == 'G') && (map[ty][tx] != 'B')) {
+				dfs_2(ty, tx, color);
+			} else if (color == map[ty][tx]) {
+				dfs_2(ty, tx, color);
 			}
 		}
 	}
 	
 	public static boolean invalidRange(int y, int x) {
 		return x < 0 || x >= N || y < 0 || y >= N;
-	}
-}
-
-class Point {
-	int x; 
-	int y;
-	char color;
-	
-	Point(int y, int x, char color) {
-		this.x = x;
-		this.y = y;
-		this.color = color;
 	}
 }
