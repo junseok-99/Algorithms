26b3fbc5ba2dc6ff524d776ed5fb9b53812003e9
장준석 <angec5689@naver.com>
2024-02-28 14:16:29 +0900

    [Silver I] Title: RGB거리, Time: 144 ms, Memory: 14596 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,46 +1,41 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int r,g,b,min=Integer.MAX_VALUE;
-
-        int N = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-
-        int[][] dp = new int[N][3];
-
-        dp[0][0] = Integer.parseInt(st.nextToken());
-        dp[0][1] = Integer.parseInt(st.nextToken());
-        dp[0][2] = Integer.parseInt(st.nextToken());
-
-        for(int i=1;i<N;i++){
-            st = new StringTokenizer(br.readLine());
-            r = Integer.parseInt(st.nextToken());
-            g = Integer.parseInt(st.nextToken());
-            b = Integer.parseInt(st.nextToken());
-            for(int j=0;j<3;j++){
-                if(j==0)
-                    dp[i][j] = Math.min(dp[i-1][1]+r ,dp[i-1][2]+r );
-                else if(j==1)
-                    dp[i][j] = Math.min(dp[i-1][0]+g ,dp[i-1][2]+g );
-                else if(j==2)
-                    dp[i][j] = Math.min(dp[i-1][0]+b ,dp[i-1][1]+b );
-
-                if(i==N-1 && dp[i][j] < min)
-                    min = dp[i][j];
-            }
-        }
-        
-        bw.write(min+"");
-        bw.flush();
-        bw.close();
-
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		
+		int[][] map = new int[N][3];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 3; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[][] dp = new int[N][3];
+		dp[0][0] = map[0][0];
+		dp[0][1] = map[0][1];
+		dp[0][2] = map[0][2];
+		
+		for (int i = 1; i < N; i++) {
+			for (int j = 0; j < 3; j++) {
+				int l = (j + 2) % 3;
+				int r = (j + 1) % 3;
+				dp[i][j] += Math.min(dp[i - 1][l], dp[i - 1][r]) + map[i][j];
+			}
+		}
+		
+		int answer = Integer.MAX_VALUE;
+		for (int i = 0; i < 3; i++) {
+			answer = Math.min(answer, dp[N - 1][i]);
+		}
+		System.out.println(answer);
+	}
 
 }
