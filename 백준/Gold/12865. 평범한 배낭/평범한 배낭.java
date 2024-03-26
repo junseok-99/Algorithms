c595abb44d478f9235b90e851fb05f038c6b36c9
장준석 <angec5689@naver.com>
2024-03-26 22:29:19 +0900

    [Gold V] Title: 평범한 배낭, Time: 228 ms, Memory: 53456 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,29 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int[] dp = new int[K+1];
+        int[][] dp = new int[N][K + 1];
+        int[][] bags = new int[N][2]; //0: weight, 1: value
 
-        for(int i=1;i<=N;i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int w = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            for(int j=K;j>=w;j--) {
-                if (dp[j] < dp[j-w] + v) {
-                    dp[j] = dp[j-w] + v;
-                }
+            bags[i][0] = Integer.parseInt(st.nextToken());
+            bags[i][1] = Integer.parseInt(st.nextToken());
+            if (i == 0) {
+                for (int j = bags[i][0]; j <= K; j++) dp[i][j] = bags[i][1];
             }
         }
-        System.out.println(dp[K]);
+
+        for (int i = 1; i < N; i++) {
+            int w = bags[i][0];
+            int v = bags[i][1];
+            for (int j = 1; j <= K; j++) {
+                if (j - w < 0) dp[i][j] = dp[i - 1][j];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
+            }
+        }
+        System.out.println(dp[N - 1][K]);
     }
 }
