e72532beaa1d90b9a8999facc77f4c8b8a44b8e3
장준석 <angec5689@naver.com>
2024-07-17 20:02:14 +0900

    [Silver I] Title: 영역 구하기, Time: 80 ms, Memory: 12276 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,80 +1,93 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main{
+public class Main {
 
-    static int[][] map;
+    static int M;
+    static int N;
+    static int K;
+    static int[][] d = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
     static boolean[][] visited;
-    public static void main(String[] args) throws IOException {
+    static List<Integer> list = new ArrayList<Integer>();
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        int M = Integer.parseInt(st.nextToken());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int cnt = 0;
-
-        map = new int[M][N];
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
         visited = new boolean[M][N];
 
-        for(int i=0;i<K;i++) {
+        for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            for(int j=x1;j<x2;j++) {
-                for(int k=y1;k<y2;k++) {
-                    map[k][j] = -1;
+            for (int j = x1; j < x2; j++) {
+                for (int k = y1; k < y2; k++) {
                     visited[k][j] = true;
                 }
             }
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
-        for(int i=0;i<M;i++) {
-            for(int j=0;j<N;j++) {
+        int areaCount = 0;
+
+
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
                 if (!visited[i][j]) {
-                    ++cnt;
-                    pq.add(bfs(i, j, M, N));
+                    ++areaCount;
+                    bfs(i, j);
                 }
             }
         }
+        Collections.sort(list);
 
-        System.out.println(cnt);
-        for(int i=0;i<cnt;i++) {
-            System.out.printf("%d ", pq.poll());
+        sb.append(areaCount + "\n");
+        for (int number : list) {
+            sb.append(number + " ");
         }
+        System.out.println(sb);
     }
 
-    public static int bfs(int y, int x, int M, int N) {
-        int cnt = 1;
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
-        Queue<int[]> q = new LinkedList<>();
+    public static boolean invalidRange(int x, int y) {
+        return x < 0 || x >= N || y < 0 || y >= M;
+    }
+    public static void bfs(int y, int x) {
+        Deque<Pos> q = new ArrayDeque<>();
+        q.add(new Pos(y, x));
+        int size = 0;
         visited[y][x] = true;
-        q.add(new int[]{y,x});
 
         while (!q.isEmpty()) {
-            int[] point = q.poll();
+            Pos p = q.poll();
+            ++size;
 
-            for(int i=0;i<4;i++) {
-                int tx = point[1] + dx[i];
-                int ty = point[0] + dy[i];
+            for (int i = 0; i < 4; i++) {
+                int tx = p.x + d[i][0];
+                int ty = p.y + d[i][1];
 
-                if (tx < 0 || tx >= N || ty < 0 || ty >= M) {
-                    continue;
-                }
-                if (!visited[ty][tx]) {
-                    visited[ty][tx] = true;
-                    q.add(new int[]{ty, tx});
-                    cnt++;
-                }
+                if (invalidRange(tx, ty) || visited[ty][tx]) continue;
+                visited[ty][tx] = true;
+                q.add(new Pos(ty, tx));
             }
         }
-        return cnt;
+        list.add(size);
     }
+}
 
+class Pos {
+    int x;
+    int y;
+
+    public Pos(int y, int x) {
+        this.x = x;
+        this.y = y;
+    }
 }
