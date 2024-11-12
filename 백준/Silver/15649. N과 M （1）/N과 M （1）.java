10d9c11cd56a39cb34298c8bf3248829710a1932
장준석 <angec5689@naver.com>
2024-11-12 12:22:28 +0900

    [Silver III] Title: N과 M (1), Time: 156 ms, Memory: 33428 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -4,35 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static StringBuilder sb = new StringBuilder();
-	static boolean[] visited;
-	static int N;
-	static int M;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		visited = new boolean[N + 1];
-		
-		dfs(0, "");
-		
-		System.out.println(sb.toString());
-	}
-	
-	public static void dfs(int depth, String s) {
-		if (depth == M) {
-			sb.append(s + "\n");
-			return;
-		}
-		
-		for (int i = 1; i <= N; i++) {
-			if (visited[i]) continue;
-			visited[i] = true;
-			dfs(depth + 1, s + i + " ");
-			visited[i] = false;
-		}
-	}
+
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static int M;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+
+        dfs(0, "");
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth, String s) {
+        if (depth == M) {
+            sb.append(s).append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            dfs(depth + 1, s + i + " ");
+            visited[i] = false;
+        }
+    }
 }
