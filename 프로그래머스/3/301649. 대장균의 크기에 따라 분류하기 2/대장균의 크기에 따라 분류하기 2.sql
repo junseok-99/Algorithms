6a73199e6f921604cac2a7285c4e1669b39a3bd0
장준석 <angec5689@naver.com>
2025-01-08 19:11:17 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,12 @@
+SELECT ID, 
+        CASE 
+            WHEN S.RANKS/CNTS <= 0.25 THEN 'CRITICAL'
+            WHEN S.RANKS/CNTS <= 0.5 THEN 'HIGH'
+            WHEN S.RANKS/CNTS <= 0.75 THEN 'MEDIUM'
+            ELSE 'LOW'
+        END AS COLONY_NAME
+    FROM (SELECT *, 
+            RANK() OVER(ORDER BY SIZE_OF_COLONY DESC) AS RANKS, 
+            COUNT(*) OVER() AS CNTS
+          FROM ECOLI_DATA) AS S
+    ORDER BY 1
