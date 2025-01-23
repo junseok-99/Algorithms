b2291378bbe549dcc7bafa48b07973ffa739f7ea
장준석 <angec5689@naver.com>
2025-01-23 18:42:42 +0900

    [Gold V] Title: 내려가기, Time: 276 ms, Memory: 44904 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -10,39 +10,40 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        int[][] map = new int[N][3];
-        int[][] minDp = new int[N][3];
-        int[][] maxDp = new int[N][3];
+        int[] minDp = new int[3];
+        int[] maxDp = new int[3];
 
         for (int i = 0; i < N; i++) {
+            int[] arr = new int[3];
+
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                arr[j] = Integer.parseInt(st.nextToken());
             }
-            Arrays.fill(minDp[i], Integer.MAX_VALUE);
-        }
 
-        minDp[0][0] = map[0][0];
-        minDp[0][1] = map[0][1];
-        minDp[0][2] = map[0][2];
-        maxDp[0][0] = map[0][0];
-        maxDp[0][1] = map[0][1];
-        maxDp[0][2] = map[0][2];
+            if (i == 0) {
+                minDp[0] = maxDp[0] = arr[0];
+                minDp[1] = maxDp[1] = arr[1];
+                minDp[2] = maxDp[2] = arr[2];
+            } else {
+                int beforeMD_0 = minDp[0];
+                int beforeMD_2 = minDp[2];
+                minDp[0] = Math.min(minDp[0], minDp[1]) + arr[0];
+                minDp[2] = Math.min(minDp[1], minDp[2]) + arr[2];
+                minDp[1] = Math.min(beforeMD_0, Math.min(minDp[1], beforeMD_2)) + arr[1];
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = -1; k <= 1; k++) {
-                    if (j + k < 0 || j + k >= 3) continue;
-                    minDp[i + 1][j + k] = Math.min(minDp[i + 1][j + k], minDp[i][j] + map[i + 1][j + k]);
-                    maxDp[i + 1][j + k] = Math.max(maxDp[i + 1][j + k], maxDp[i][j] + map[i + 1][j + k]);
-                }
+                int beforeMXD_0 = maxDp[0];
+                int beforeMXD_2 = maxDp[2];
+                maxDp[0] = Math.max(maxDp[0], maxDp[1]) + arr[0];
+                maxDp[2] = Math.max(maxDp[1], maxDp[2]) + arr[2];
+                maxDp[1] = Math.max(beforeMXD_0, Math.max(maxDp[1], beforeMXD_2)) + arr[1];
             }
         }
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < 3; i++) {
-            min = Math.min(min, minDp[N - 1][i]);
-            max = Math.max(max, maxDp[N - 1][i]);
+            min = Math.min(min, minDp[i]);
+            max = Math.max(max, maxDp[i]);
         }
         System.out.println(max + " " + min);
     }
