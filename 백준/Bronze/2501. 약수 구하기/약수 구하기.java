d97aa547c410d77989cc67869d24dbccd626ed79
장준석 <angec5689@naver.com>
2026-03-24 23:17:46 +0900

    [Bronze III] Title: 약수 구하기, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -4,18 +4,18 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int cnt = 0;
 
+        int cnt = 0;
         for (int i = 1; i <= N; i++) {
-            if (N % i == 0) cnt++;
-            if (K == cnt) {
+            if (N % i == 0) ++cnt;
+            if (cnt == K) {
                 System.out.println(i);
                 return;
             }
